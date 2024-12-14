12de318f6de08354a900a2d7e994ee15d63cc2f0
FM102DX <78497211+FM102DX@users.noreply.github.com>
2024-12-14 12:03:24 +0300

    Сделал базовые кнопки загрузки ассортимента и подключл радзен

---
+++ ControlNode/Pages/Assortment.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace ControlNode.Pages
+{
+    public partial class Assortment
+    {
+        private string _message;
+        public Assortment()
+        {
+            
+        }
+
+        private void OnLoadAssortClick()
+        {
+            _message = "Ok!";
+        }
+    }
+}
