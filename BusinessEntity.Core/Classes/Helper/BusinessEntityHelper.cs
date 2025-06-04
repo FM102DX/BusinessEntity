531195f9bc903adf36884f78c3472f08f886dbf8
FM102DX <78497211+FM102DX@users.noreply.github.com>
2025-06-04 19:09:11 +0300

    Made new systems classes

---
+++ BusinessEntity.Core/Classes/Helper/BusinessEntityHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.Core.Classes.Helper
+{
+    public class BusinessEntityHelper
+    {
+
+    }
+}
